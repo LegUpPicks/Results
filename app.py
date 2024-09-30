a3ead37ea588541e73cd81d2d9c04e5947abd00e
ChaseRomano <info@leguppicks.com>
2024-09-30 08:15:34 -0400

    Fix Sum Stats

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ import numpy as np
 from streamlit_gsheets import GSheetsConnection
 
 st.set_page_config(layout="wide")
+
 url = "https://docs.google.com/spreadsheets/d/1XoVHcy6qqwKKT7HiIb5CKwv32_1Ce1fhl5XoPW-lREI/edit?usp=sharing"
 
 conn = st.connection("gsheets", type=GSheetsConnection)
@@ -35,11 +36,25 @@ win_percentage = (w_count / total_records) * 100 if total_records > 0 else 0
 total_units = df['Units_W_L'].sum()
 
 # Display summary statistics with rounding
+st.image('legup.png', width = 200)
 st.header("Summary Statistics")
-st.write(f"Total Wins: {w_count}, Total Losses: {l_count}, Total Pushes: {p_count}")
-st.write(f"Win Percentage: {win_percentage:.2f}%")
-st.write(f"Total Units: {total_units:.2f}")
 
+col1, col2, col3, col4, col5 = st.columns(5)
+
+with col1:
+    st.metric("Total Wins", w_count)
+    
+with col2:
+    st.metric("Total Losses", l_count)
+
+with col3:
+    st.metric("Total Pushes", p_count)
+
+with col4:
+    st.metric("Win Percentage", f"{win_percentage:.2f}%")
+
+with col5:
+    st.metric("Total Units", f"{total_units:.2f}")
 # Cumulative units line chart
 df_cumulative = df.groupby('Date').agg({'Units_W_L': 'sum'}).cumsum().reset_index()
 df_cumulative.rename(columns={'Units_W_L': 'Units'}, inplace=True)
@@ -66,4 +81,7 @@ calendar_data['Date'] = pd.to_datetime(calendar_data['Date'])
 df = df.sort_values(by='Date', ascending=False)
 
 st.header('Full Data')
+# Format the Date column
+df['Date'] = df['Date'].dt.strftime('%m/%d/%Y')
+# Display the DataFrame
 st.dataframe(df)
